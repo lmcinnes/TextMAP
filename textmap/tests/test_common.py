a9adf2902febfedbad5a773a3c3d5372aeb1c863
Leland McInnes <leland.mcinnes@gmail.com>
2020-03-04 13:48:32 -0500

    Update tests to match reality

--- textmap/tests/test_common.py
+++ textmap/tests/test_common.py
@@ -2,13 +2,13 @@ import pytest
 
 from sklearn.utils.estimator_checks import check_estimator
 
-from textmap import TemplateEstimator
-from textmap import TemplateClassifier
-from textmap import TemplateTransformer
+from textmap import WordMAP
+from textmap import DocMAP
+from textmap import TopicMAP
 
 
 @pytest.mark.parametrize(
-    "Estimator", [TemplateEstimator, TemplateTransformer, TemplateClassifier]
+    "Estimator", [WordMAP, DocMAP, TopicMAP]
 )
 def test_all_estimators(Estimator):
     return check_estimator(Estimator)
