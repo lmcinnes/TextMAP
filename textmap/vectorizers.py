99797365c1c28cb3b73ca18c684ecba7a141b2fd
John Healy <jcheayl@gmail.com>
2020-03-20 18:05:34 -0400

    refactored the utilities functions into a transformer and vetorizer file as well as adding some rough notes to how we'd like our docmap and topicmap classes to work.

---
+++ textmap/vectorizers.py
@@ -0,0 +1,25 @@
+
+class WordVectorizer (BaseEstimator, TransformerMixin):
+    def __init__(self):
+        # make sure we pass the before/after as a switch
+        pass
+
+    def fit(self):
+        # use tokenizers to build list of lists
+        # co-occurence vectorizer
+        # information transformer
+        # em transformer
+        pass
+    pass
+
+class DocVectorizer (BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+
+    def fit(self):
+        # use tokenizers to build list of lists
+        # NgramVectorizer
+        # information transformer
+        # Em transformer
+        pass
+    pass
