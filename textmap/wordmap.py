87f1adb99b92fe4110856892d07890cb1b32dc24
John Healy <jcheayl@gmail.com>
2020-03-23 10:40:45 -0400

    refactored the utilities functions into a transformer and vetorizer file as well as adding some rough notes to how we'd like our docmap and topicmap classes to work.  Fixed a typo that was preventing import.

--- textmap/wordmap.py
+++ textmap/wordmap.py
@@ -3,9 +3,5 @@ from sklearn.base import BaseEstimator, ClassifierMixin, TransformerMixin
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 
 class WordMAP (BaseEstimator, TransformerMixin):
-    def __init__(self):
 
-    def fit(self):
-        # use tokenizers to build list of lists
-        count_matrix =
     pass
