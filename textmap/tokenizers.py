6662a3401e80f45f7f558e90952e7dd341b6d65d
Colin Weir <colin.weir@learnleapfly.org>
2020-03-25 16:27:39 -0400

    Fixed number of bigram contration loops and added CountVectorizerTokenizer

--- textmap/tokenizers.py
+++ textmap/tokenizers.py
@@ -4,6 +4,7 @@ from nltk.tokenize import sent_tokenize, word_tokenize, MWETokenizer
 from nltk.collocations import BigramCollocationFinder
 from nltk.metrics import BigramAssocMeasures
 
+from sklearn.feature_extraction.text import CountVectorizer
 
 class BaseTokenizer(BaseEstimator, TransformerMixin):
     """
@@ -80,15 +81,67 @@ class NLTKTokenizer(BaseTokenizer):
 
       Parameters
       ----------
-      max_MWE_iterations: int
+      lower_case = bool (default = False)
+        Lower-case the sentences before tokenization.
+
+      max_MWE_iterations = int (default = 2)
         The maximal number of recursive bigram contractions
 
-      min_MWE_PMIL: int
+      min_MWE_PMIL: int (default = 12)
         The minimal PMI value to contract a bigram per iteration
 
     """
 
-    def __init__(self, max_MWE_iterations=2, min_MWE_PMI=10):
+    def __init__(self, lower_case = False, max_MWE_iterations=2, min_MWE_PMI=12):
+
+        BaseTokenizer.__init__(self)
+        self.max_MWE_iterations = max_MWE_iterations
+        self.min_MWE_PMI = min_MWE_PMI
+        self.lower_case = lower_case
+
+    def fit(self, X, **fit_params):
+        """
+
+        Parameters
+        ----------
+        X: collection
+            The list of documents
+
+        Returns
+        -------
+        self
+        """
+        if self.lower_case:
+            self.tokens_by_sent_by_doc_ = [
+                [word_tokenize(sent.lower()) for sent in sent_tokenize(doc)] for doc in X
+            ]
+        else:
+            self.tokens_by_sent_by_doc_ = [
+                [word_tokenize(sent) for sent in sent_tokenize(doc)] for doc in X
+            ]
+
+        for i in range(self.max_MWE_iterations):
+            bigramer = BigramCollocationFinder.from_documents(self.tokens_by_sent())
+            mwes = list(
+                bigramer.above_score(
+                    BigramAssocMeasures.likelihood_ratio, self.min_MWE_PMI
+                )
+            )
+            if len(mwes) == 0:
+                break
+            contracter = MWETokenizer(mwes)
+            self.tokens_by_sent_by_doc_ = [
+                contracter.tokenize_sents(doc) for doc in self.tokens_by_sent_by_doc()
+            ]
+        return self
+
+
+class CountVectorizerTokenizer(BaseTokenizer):
+    '''
+    Uses CountVectorizers document preprocessing and word tokenizer (but NLTK sentence tokenizer) and otherwise behaves
+        as NLTKTokenizer
+    '''
+    def __init__(self, max_MWE_iterations=2, min_MWE_PMI=12):
 
         BaseTokenizer.__init__(self)
         self.max_MWE_iterations = max_MWE_iterations
@@ -106,10 +159,12 @@ class NLTKTokenizer(BaseTokenizer):
         -------
         self
         """
+        cv_word_tokenize = CountVectorizer().build_tokenizer()
+        cv_preprocesser = CountVectorizer().build_preprocessor()
         self.tokens_by_sent_by_doc_ = [
-            [word_tokenize(sent) for sent in sent_tokenize(doc)] for doc in X
+            [cv_word_tokenize(sent) for sent in sent_tokenize(cv_preprocesser(doc))] for doc in X
         ]
-        for i in range(1, self.max_MWE_iterations):
+        for i in range(self.max_MWE_iterations):
             bigramer = BigramCollocationFinder.from_documents(self.tokens_by_sent())
             mwes = list(
                 bigramer.above_score(
@@ -125,9 +180,8 @@ class NLTKTokenizer(BaseTokenizer):
         return self
 
 
-class CountVectorizerTokenizer(BaseTokenizer):
-    pass
-
-
 class StanzaTokenizer(BaseTokenizer):
     pass
+
+
+
