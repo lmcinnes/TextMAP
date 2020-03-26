3c3a8e695e65e81fa03a8646a9fa2732715e1c3d
Leland McInnes <leland.mcinnes@gmail.com>
2020-03-26 14:00:15 -0400

    Add a minimal test

--- textmap/tests/test_common.py
+++ textmap/tests/test_common.py
@@ -1,10 +1,13 @@
 import pytest
 
 from sklearn.utils.estimator_checks import check_estimator
+import scipy.sparse
 
 from textmap import WordMAP
 from textmap import DocMAP
 from textmap import TopicMAP
+
+from textmap.vectorizers import WordVectorizer
 
 
 # @pytest.mark.parametrize(
@@ -12,3 +15,15 @@ from textmap import TopicMAP
 # )
 # def test_all_estimators(Estimator):
 #     return check_estimator(Estimator)
+
+test_text = [
+    "foo bar pok wer pok pok foo bar wer qwe pok asd fgh",
+    "fgh asd foo pok qwe pok wer pok foo bar pok pok wer",
+    "pok wer pok qwe foo asd foo bar pok wer asd wer pok",
+]
+
+def test_wordvectorizer_basic():
+    vectorizer = WordVectorizer()
+    result = vectorizer.fit(test_text)
+    assert scipy.sparse.issparse(result)
+    assert result.shape == (7, 14)
