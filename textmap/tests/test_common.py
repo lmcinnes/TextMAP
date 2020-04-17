17bb37eb2c6c6fbd5422a99330760c2c165537a1
John Healy <jcheayl@gmail.com>
2020-04-17 15:57:02 -0400

    Blacked the code.

--- textmap/tests/test_common.py
+++ textmap/tests/test_common.py
@@ -51,35 +51,33 @@ test_text_token_data = (
     ),
 )
 
-test_matrix = scipy.sparse.csr_matrix([ [ 1, 2 ,3], [4,5,6], [7,8,9] ])
-test_matrix_zero_row = scipy.sparse.csr_matrix([ [ 1, 2 ,3], [4,5,6], [0,0,0] ])
+test_matrix = scipy.sparse.csr_matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+test_matrix_zero_row = scipy.sparse.csr_matrix([[1, 2, 3], [4, 5, 6], [0, 0, 0]])
 test_matrix_zero_row.eliminate_zeros()
-test_matrix_zero_column = scipy.sparse.csr_matrix([ [ 1, 2 ,0], [4,5,0], [7,8,0] ])
+test_matrix_zero_column = scipy.sparse.csr_matrix([[1, 2, 0], [4, 5, 0], [7, 8, 0]])
 test_matrix_zero_column.eliminate_zeros()
 
 
-
-def test_featurebasistransformer_tokenized(
-):
-    model = FeatureBasisTransformer(vectorizer='tokenized', n_components=3)
+def test_featurebasistransformer_tokenized():
+    model = FeatureBasisTransformer(vectorizer="tokenized", n_components=3)
     result = model.fit_transform(test_text_token_data)
     assert result.shape == (4, 3)
-    #transform = model.transform(test_text_token_data)
-    #assert (result != transform).nnz == 0
+    # transform = model.transform(test_text_token_data)
+    # assert (result != transform).nnz == 0
+
 
 def test_wordvectorizer_todataframe():
     model = WordVectorizer().fit(test_text)
     df = model.to_DataFrame()
     assert df.shape == (7, 14)
 
+
 # Should we also test for stanza?  It's failing in Travis.
 @pytest.mark.parametrize("tokenizer", ["nltk", "tweet", "spacy", "sklearn"])
 @pytest.mark.parametrize("token_contractor", ["aggressive", "conservative"])
 @pytest.mark.parametrize("vectorizer", ["bow", "bigram"])
 @pytest.mark.parametrize("normalize", [True, False])
-def test_docvectorizer_basic(
-        tokenizer, token_contractor, vectorizer, normalize
-):
+def test_docvectorizer_basic(tokenizer, token_contractor, vectorizer, normalize):
     model = DocVectorizer(
         tokenizer=tokenizer,
         token_contractor=token_contractor,
@@ -89,15 +87,17 @@ def test_docvectorizer_basic(
     result = model.fit_transform(test_text)
     transform = model.transform(test_text)
     assert (result != transform).nnz == 0
-    if vectorizer == 'bow':
+    if vectorizer == "bow":
         assert result.shape == (5, 7)
-    if vectorizer == 'bigram':
+    if vectorizer == "bigram":
         assert result.shape == (5, 19)
+
 
 def test_docvectorizer_todataframe():
     model = DocVectorizer().fit(test_text)
     df = model.to_DataFrame()
     assert df.shape == (5, 7)
+
 
 # Should we also test for stanza?  Stanza's pytorch dependency makes this hard.
 @pytest.mark.parametrize("tokenizer", ["nltk", "tweet", "spacy", "sklearn"])
@@ -121,4 +121,3 @@ def test_wordvectorizer_basic(
     if vectorizer == "flat_1_5":
         assert result.shape == (7, 28)
     assert type(result) == scipy.sparse.csr.csr_matrix
-
