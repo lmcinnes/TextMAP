b9a0ae8846ba5f1c121040d4ad4ed48644604643
John Healy <jcheayl@gmail.com>
2020-04-17 15:54:37 -0400

    Added a FeatureBasisTransformer and some unit testing.

--- textmap/tests/test_common.py
+++ textmap/tests/test_common.py
@@ -7,7 +7,7 @@ from textmap import WordMAP
 from textmap import DocMAP
 from textmap import TopicMAP
 
-from textmap.vectorizers import DocVectorizer, WordVectorizer
+from textmap.vectorizers import DocVectorizer, WordVectorizer, FeatureBasisTransformer
 
 import nltk
 
@@ -28,11 +28,49 @@ test_text = [
     "pok wer pok qwe foo asd foo bar pok wer asd wer pok",
 ]
 
+test_text_token_data = (
+    ("foo", "pok", "foo", "wer", "bar"),
+    (),
+    ("bar", "foo", "bar", "pok", "wer", "foo", "bar", "foo", "pok", "bar", "wer"),
+    ("wer", "foo", "foo", "pok", "bar", "wer", "bar"),
+    ("foo", "bar", "bar", "foo", "bar", "foo", "pok", "wer", "pok", "bar", "wer"),
+    ("pok", "wer", "bar", "foo", "pok", "foo", "wer", "wer", "foo", "pok", "bar"),
+    (
+        "bar",
+        "foo",
+        "pok",
+        "foo",
+        "wer",
+        "wer",
+        "foo",
+        "wer",
+        "foo",
+        "pok",
+        "bar",
+        "wer",
+    ),
+)
+
 test_matrix = scipy.sparse.csr_matrix([ [ 1, 2 ,3], [4,5,6], [7,8,9] ])
 test_matrix_zero_row = scipy.sparse.csr_matrix([ [ 1, 2 ,3], [4,5,6], [0,0,0] ])
 test_matrix_zero_row.eliminate_zeros()
 test_matrix_zero_column = scipy.sparse.csr_matrix([ [ 1, 2 ,0], [4,5,0], [7,8,0] ])
 test_matrix_zero_column.eliminate_zeros()
+
+
+
+def test_featurebasistransformer_tokenized(
+):
+    model = FeatureBasisTransformer(vectorizer='tokenized', n_components=3)
+    result = model.fit_transform(test_text_token_data)
+    assert result.shape == (4, 3)
+    #transform = model.transform(test_text_token_data)
+    #assert (result != transform).nnz == 0
+
+def test_wordvectorizer_todataframe():
+    model = WordVectorizer().fit(test_text)
+    df = model.to_DataFrame()
+    assert df.shape == (7, 14)
 
 # Should we also test for stanza?  It's failing in Travis.
 @pytest.mark.parametrize("tokenizer", ["nltk", "tweet", "spacy", "sklearn"])
@@ -56,6 +94,10 @@ def test_docvectorizer_basic(
     if vectorizer == 'bigram':
         assert result.shape == (5, 19)
 
+def test_docvectorizer_todataframe():
+    model = DocVectorizer().fit(test_text)
+    df = model.to_DataFrame()
+    assert df.shape == (5, 7)
 
 # Should we also test for stanza?  Stanza's pytorch dependency makes this hard.
 @pytest.mark.parametrize("tokenizer", ["nltk", "tweet", "spacy", "sklearn"])
@@ -79,3 +121,4 @@ def test_wordvectorizer_basic(
     if vectorizer == "flat_1_5":
         assert result.shape == (7, 28)
     assert type(result) == scipy.sparse.csr.csr_matrix
+
---
+++ textmap/tests/test_transformers.py
@@ -0,0 +1,7 @@
+import pytest
+from textmap.transformers import FeatureBasisTransformer
+
+
+
+test_matrix = scipy.sparse.csr_matrix([ [ 1, 2 ,3], [4,5,6], [7,8,9] ])
+
